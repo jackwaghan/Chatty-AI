5c50d1bada0868520bf17e5860027c5862e49aef
Jack Waghan <jackwaghan@gmail.com>
2025-03-04 00:17:05 +0530

    Revert "Refactor SEO metadata into separate utility files for improved organization and maintainability; add aria-label to Github link for accessibility"
    This reverts commit b8f7932d4bf3e2dfb5f4a0d139d63660e8cb2fd4.

--- app/components/Github.tsx
+++ app/components/Github.tsx
@@ -5,7 +5,6 @@ import { Github as GithubImage } from "lucide-react";
 const Github = () => {
   return (
     <Link
-      aria-label="Git Repo Link"
       href="https://github.com/jackwaghan/Chatty-AI"
       className="px-2 py-1.5 border border-white/10 rounded-lg bg-[#1A1C1E] cursor-pointer hover:scale-95 duration-200"
       target="_blank"
--- utils/Seo/Url.ts
+++
@@ -1,4 +0,0 @@
-export const ImageURL =
-  "https://res.cloudinary.com/dqswovyzi/image/upload/v1740592387/Landing_page_zsgit4.png";
-
-export const DomainURL = "https://chatty.jackwaghan.com";
